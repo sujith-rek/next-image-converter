205a2d1ba3ad836a28f81a1c27eb2592dad99c3b
Sujith <ps335@snu.edu.in>
2023-09-21 00:18:41 +0530

    add converters

---
+++ change_names.py
@@ -0,0 +1,30 @@
+import os
+import re
+
+def change_data_of_file(file_path):
+    # read file
+    file = open(file_path, 'r')
+    data = file.read()
+    file.close()
+
+    # change data
+    data = re.sub(r'\.png', '.webp', data)
+    data = re.sub(r'\.jpeg', '.webp', data)
+    data = re.sub(r'\.jpg', '.webp', data)
+
+    # write file
+    file = open(file_path, 'w')
+    file.write(data)
+    file.close()
+
+def change_data_of_dir(dir_path):
+    # crawl through all files and folders of dir_path
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if file.isdir():
+                change_data_of_dir(file)
+            else:
+                change_data_of_file(file)
+
+file_path = os.getcwd()
+change_data_of_dir(file_path)
---
+++ convert_image.py
@@ -0,0 +1,27 @@
+import os
+from PIL import Image
+
+def change_images_of_directory(directory):
+    for filename in os.listdir(directory):
+        # if filename is directory, then call this function recursively
+        if os.path.isdir(filename):
+            change_images_of_directory(filename)
+        else:
+            # if filename is image, then change it
+            if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
+                change_image(filename)
+
+def change_image(filename):
+    img = Image.open(filename)
+    # change to webp
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        filename = filename[:-4] + ".webp"
+    elif filename.endswith(".jpeg"):
+        filename = filename[:-5] + ".webp"
+    img.save(filename, "webp")
+    # delete old image
+    os.remove(filename)
+
+current_dir = os.getcwd()
+change_images_of_directory(current_dir)
+
