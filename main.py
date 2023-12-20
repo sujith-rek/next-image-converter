161644890feb947f1e5859fb456fb3c2cab7cc4e
Sujith <ps335@snu.edu.in>
2023-12-20 23:03:12 +0530

    full file script

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import re
 from PIL import Image
+
 
 def change_images_of_directory(directory):
     for filename in os.listdir(directory):
@@ -10,9 +12,9 @@ def change_images_of_directory(directory):
             # if filename is image, then change it
             if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
                 change_image(os.path.join(directory, filename))
-    
+
     delete_files_of_directory(directory)
-    
+
 
 def delete_files_of_directory(directory):
     for filename in os.listdir(directory):
@@ -24,6 +26,7 @@ def delete_files_of_directory(directory):
             if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
                 os.remove(filename)
 
+
 def change_image(filename):
     img = Image.open(filename)
     # change to webp
@@ -34,3 +37,40 @@ def change_image(filename):
     img.save(filename, "webp")
 
 
+def change_data_of_file(file_path):
+    # read file
+    file = open(file_path, 'r')
+    data = file.read()
+    file.close()
+
+    # change data
+    data = re.sub(r'\.png', '.webp', data)
+    data = re.sub(r'\.jpeg', '.webp', data)
+    data = re.sub(r'\.jpg', '.webp', data)
+
+    # write file
+    file = open(file_path, 'w')
+    file.write(data)
+    file.close()
+
+
+def change_data_of_dir(dir_path):
+    # crawl through all files and folders of dir_path
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if os.path.isdir(file):
+                change_data_of_dir(os.path.join(root, file))
+            else:
+                change_data_of_file(os.path.join(root, file))
+
+
+def convert_main():
+    current_dir = os.getcwd()
+    # convert images
+    change_images_of_directory(current_dir)
+    # change data
+    change_data_of_dir(current_dir)
+
+
+if __name__ == "__main__":
+    convert_main()
