2269a9f2e5b688bd4ae99e7f8694f225b841317a
Sujith <89125023+sujith-rek@users.noreply.github.com>
2024-01-24 21:56:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def change_images_of_directory(directory):
             change_images_of_directory(os.path.join(directory, filename))
         else:
             # if filename is image, then change it
-            if filename.endswith(".jpeg") or filename.endswith(".png") or filename.endswith(".jpg"):
+            if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
                 change_image(os.path.join(directory, filename))
 
     delete_files_of_directory(directory)
@@ -23,14 +23,14 @@ def delete_files_of_directory(directory):
             delete_files_of_directory(os.path.join(directory, filename))
         else:
             # if filename is image, then delete it
-            if filename.endswith(".jpeg") or filename.endswith(".png") or filename.endswith(".jpg"):
-                os.remove(os.path.join(directory, filename))
+            if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
+                os.remove(filename)
 
 
 def change_image(filename):
     img = Image.open(filename)
     # change to webp
-    if filename.endswith(".png") or filename.endswith(".jpg"):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
         filename = filename[:-4] + ".webp"
     elif filename.endswith(".jpeg"):
         filename = filename[:-5] + ".webp"
@@ -39,14 +39,16 @@ def change_image(filename):
 
 def change_data_of_file(file_path):
     # read file
+    print(file_path)
     file = open(file_path, 'r')
+
     data = file.read()
     file.close()
 
     # change data
-    data = re.sub(r'\.jpeg', '.webp', data)
     data = re.sub(r'\.jpg', '.webp', data)
     data = re.sub(r'\.png', '.webp', data)
+    data = re.sub(r'\.jpeg', '.webp', data)
 
     # write file
     file = open(file_path, 'w')
@@ -60,14 +62,15 @@ def change_data_of_dir(dir_path):
         for file in files:
             if os.path.isdir(file):
                 change_data_of_dir(os.path.join(root, file))
-            elif file.endswith(".js") or file.endswith(".jsx"):
-                change_data_of_file(os.path.join(root, file))
+            else:
+                if file.endswith(".js") or file.endswith(".jsx") or file.endswith(".ts") or file.endswith(".tsx") or file.endswith(".html") or file.endswith(".css"):
+                    change_data_of_file(os.path.join(root, file))
 
 
 def convert_main():
     current_dir = os.getcwd()
     # convert images
-    # change_images_of_directory(current_dir)
+    change_images_of_directory(current_dir)
     # change data
     change_data_of_dir(current_dir)
 
